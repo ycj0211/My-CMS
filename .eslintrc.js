790e97a9c4743433b53d223edef5d485e0ae2e7e
2168009464@qq.com <2168009464@qq.com>
2023-01-05 14:30:20 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: 闫程静 2168009464@qq.com
+ * @Date: 2022-12-26 16:20:31
+ * @LastEditors: 闫程静 2168009464@qq.com
+ * @LastEditTime: 2022-12-26 18:20:45
+ * @FilePath: \vue3-ts-cms\.eslintrc.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    // '@vue/prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: 闫程静 2168009464@qq.com
+ * @Date: 2022-12-26 16:20:31
+ * @LastEditors: 闫程静 2168009464@qq.com
+ * @LastEditTime: 2022-12-26 17:02:05
+ * @FilePath: \vue3-ts-cms\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
